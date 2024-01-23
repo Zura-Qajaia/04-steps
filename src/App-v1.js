07161f21376d13a5b3c2b7f23aa6a16c3948864a
Zura Kajaia <seperate1004@gmaail.com>
2024-01-23 14:04:59 +0400

    zura

--- src/App-v1.js
+++ src/App-v1.js
@@ -9,9 +9,7 @@ export default function App() {
   return (
     <div>
       <Steps />
-      {/*
       <Steps />
-  */}
     </div>
   );
 }
@@ -48,26 +46,22 @@ function Steps() {
             Step {step}: {messages[step - 1]}
           </p>
           <div className="buttons">
-            <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
-              <span>👈</span>Previous
-            </Button>
-            <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
-              Next <span>👉</span>
-            </Button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+              onMouseEnter={() => alert("Test")}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
           </div>
         </div>
       )}
     </div>
-  );
-}
-
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-    <button
-      style={{ backgroundColor: "#7950f2", color: "#fff" }}
-      onClick={onClick}
-    >
-      {children}
-    </button>
   );
 }
